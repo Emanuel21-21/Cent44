71130cad1140751355205b0fb8436d7e50bd61b4
harryenlaweb <harryenlaweb@hotmail.com>
2021-09-21 21:17:20 -0300

    21-09-2021

--- client/components/mesas/mesa_form.js
+++ client/components/mesas/mesa_form.js
@@ -4,6 +4,7 @@ import SimpleSchema from 'simpl-schema';
 import { Carreras } from '../../../lib/collections/carreras';
 import { Docentes } from '../../../lib/collections/docentes';
 import { Llamados } from '../../../lib/collections/llamados';
+import { Mesas } from '../../../lib/collections/mesas';
 // Required AutoForm setup
 SimpleSchema.extendOptions(['autoform']);
 
@@ -31,7 +32,7 @@ Template.mesaForm.helpers({
 		Template.instance().selLlamado.set(suggestion.id);  		
 	},
 	llamados1: function() {     
-		return Llamados.find().fetch().map(function(object){ return {id: object._id, value: object.llamadoNumero}; });    
+		return Llamados.find().fetch().map(function(object){ return {id: object._id, value: object.numero}; });    
 	},
 
 	selecMateria: function(event, suggestion, datasetName) {
@@ -63,20 +64,22 @@ Template.mesaForm.events({
 		event.preventDefault();
 
 		// Get value from form element
-		const target = event.target;	
+		const target = event.target;			
 		var ingresoLlamado = null;
 		var ingresoMateria = null;
 		var ingresoPresidente = null;
 		var ingresoVocal1 = null;
 		var ingresoVocal2 = null;	
-		var ingresoDescripcion = null;	
+		var ingresoDescripcion = null;
+		var ingresoMesa = null;			
 		
 		ingresoLlamado = Template.instance().selLlamado.get();
-		ingresoMateria = Template.instance().selMateria.get();      
+		ingresoMateria = Template.instance().selMateria.get();
+		
 
 		ingresoPresidente = Template.instance().selPresidente.get(); 	
 		ingresoVocal1 = Template.instance().selVocal1.get(); 	
-		ingresoVocal2 = Template.instance().selVocal2.get(); 	
+		ingresoVocal2 = Template.instance().selVocal2.get(); 			
 		
 		if (target.descripcion.value){ingresoDescripcion = target.descripcion.value};					
 
@@ -86,40 +89,78 @@ Template.mesaForm.events({
 		var llamadoSeleccionado = Llamados.findOne({"_id":ingresoLlamado});
 		if (llamadoSeleccionado){
 			idLlamado = llamadoSeleccionado._id;
-			nombreLlamdo = llamadoSeleccionado.nombre;
+			nombreLlamado = llamadoSeleccionado.nombre;
 			numeroLlamado = llamadoSeleccionado.numero;			
 		} 
 
 		var idCarrera;	
 		var nombreCarrera;	
-		var carreraSeleccionada = Carreras.findOne({"_id":ingresoCarrera});//obtengo la Carrera seleccionada (objeto)     
-		if (carreraSeleccionada){
-			idCarrera = carreraSeleccionada._id;	
-			nombreCarrera = carreraSeleccionada.nombre;		
+		var idMateria;			
+		var nombreMateria;		
+		
+		var materiaSeleccionada = Materias.findOne({"_id":ingresoMateria});//obtengo la Carrera seleccionada (objeto)     		
+		if (materiaSeleccionada){		
+			
+			idMateria = materiaSeleccionada._id;	
+			nombreMateria = materiaSeleccionada.nombre;	
+
+			idCarrera = materiaSeleccionada.idCarrera;	
+			nombreCarrera = materiaSeleccionada.nombreCarrera;
 		} 
 
-		var idDocente;		
-		var nombreDocente;
-		var dniDocente;
-		var docenteSeleccionada = Docentes.findOne({"_id":ingresoDocente});//obtengo el docente seleccionado (objeto)     
-		if (docenteSeleccionada){
-			idDocente = docenteSeleccionada._id;			
-			nombreDocente = docenteSeleccionada.nombreApellido;
-			dniDocente = docenteSeleccionada.dni;
+		var idPresidente;		
+		var nombrePresidente;
+		var dniPresidente;
+		var presidenteSeleccionada = Docentes.findOne({"_id":ingresoPresidente});//obtengo el docente seleccionado (objeto)     
+		if (presidenteSeleccionada){
+			idPresidente = presidenteSeleccionada._id;			
+			nombrePresidente = presidenteSeleccionada.nombreApellido;
+			dniPresidente = presidenteSeleccionada.dni;
 		} 
+		
+		var idVocal1;		
+		var nombreVocal1;
+		var dniVocal1;
+		var vocal1Seleccionada = Docentes.findOne({"_id":ingresoVocal1});//obtengo el docente seleccionado (objeto)     
+		if (vocal1Seleccionada){
+			idVocal1 = vocal1Seleccionada._id;			
+			nombreVocal1 = vocal1Seleccionada.nombreApellido;
+			dniVocal1 = vocal1Seleccionada.dni;
+		}
+
+		var idVocal2;		
+		var nombreVocal2;
+		var dniVocal2;
+		var vocal2Seleccionada = Docentes.findOne({"_id":ingresoVocal2});//obtengo el docente seleccionado (objeto)     
+		if (vocal2Seleccionada){
+			idVocal2 = vocal2Seleccionada._id;			
+			nombreVocal2 = vocal2Seleccionada.nombreApellido;
+			dniVocal2 = vocal2Seleccionada.dni;
+		}
 	  	
-		Mesas.insert({
+		Mesas.insert({	
 			
 
-			nombre:ingresoNombre, 			
-			idCarrera:idCarrera,
-			nombreCarrera:nombreCarrera,			
-			idDocente:idDocente,		
-			nombreDocente:nombreDocente,
-			dniDocente:dniDocente,	
-			descripcion:ingresoDescripcion,			
+			idLlamado:idLlamado, 
+			nombreLlamado:nombreLlamado,
+			numeroLlamado:numeroLlamado,
+			
+			idMateria:idMateria,	
+			nombreMateria:nombreMateria,
+			
+			idCarrera:idCarrera,	
+			nombreCarrera:nombreCarrera,
+			
+			idPresidente:idPresidente,			
+			nombrePresidente:nombrePresidente,
+
+			idVocal1:idVocal1,
+			nombreVocal1:nombreVocal1,
+
+			idVocal2:idVocal2,
+			nombreVocal2:nombreVocal2,			
+					
 		});
-		
 			
 	    Router.go('/mesas');
 		}
